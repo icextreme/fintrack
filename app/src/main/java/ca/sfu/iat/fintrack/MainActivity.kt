c40850b012aeea83fef5bbcceaed4c96f264b6d3
icextreme <74882450+icextreme@users.noreply.github.com>
2022-02-24 03:41:40 -0800

    Add new activities and update gradle
    Add new activities, update user interface, update gradle

--- app/src/main/java/ca/sfu/iat/fintrack/MainActivity.kt
+++ app/src/main/java/ca/sfu/iat/fintrack/MainActivity.kt
@@ -1,11 +1,23 @@
 package ca.sfu.iat.fintrack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ca.sfu.iat.fintrack.databinding.ActivityMainBinding
+import ca.sfu.iat.fintrack.view.RegisterActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/ca/sfu/iat/fintrack/view/RegisterActivity.kt
@@ -0,0 +1,18 @@
+package ca.sfu.iat.fintrack.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ca.sfu.iat.fintrack.R
+import ca.sfu.iat.fintrack.databinding.ActivityMainBinding
+import ca.sfu.iat.fintrack.databinding.ActivityRegisterBinding
+import ca.sfu.iat.fintrack.databinding.ActivitySigninBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
