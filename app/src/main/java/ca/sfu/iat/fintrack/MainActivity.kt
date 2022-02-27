1dbe5d1144f9826a70cd5db22b8c676a9359d7a0
truongnguyenlinh <ltruong28@my.bcit.ca>
2022-02-26 23:25:47 -0800

    Add top and bottom navbars

--- app/src/main/java/ca/sfu/iat/fintrack/MainActivity.kt
+++ app/src/main/java/ca/sfu/iat/fintrack/MainActivity.kt
@@ -3,6 +3,9 @@ package ca.sfu.iat.fintrack
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import ca.sfu.iat.fintrack.databinding.ActivityMainBinding
 import ca.sfu.iat.fintrack.view.RegisterActivity
 
@@ -13,11 +16,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.elevation = 0F
+        supportActionBar?.setCustomView(R.layout.toolbar_title_layout)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.textView.setOnClickListener {
-            startActivity(Intent(this, RegisterActivity::class.java))
-        }
+        val navController = findNavController(R.id.nav_fragment)
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
