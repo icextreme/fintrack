3bb018a7dfd039de24d55ad7b799a1ed770bdde1
truongnguyenlinh <46574403+truongnguyenlinh@users.noreply.github.com>
2022-03-01 17:43:27 -0800

    Update app flow  (#2)
    * Add fonts
    
    Add fonts
    
    * Add new activities and update gradle
    
    Add new activities, update user interface, update gradle
    
    * Remove misc.xml
    
    Remove misc.xml
    
    * Fix build errors
    
    * Add top and bottom navbars
    
    * Add login page
    
    * add graphs on main
    
    * Update linear layout weights
    
    * update UI activitymain
    
    * change filenames
    
    * add graphs to navfragment
    
    * Create add entry layout
    
    * Fix regex in .gitignore
    
    * rename files
    
    * update component orientation
    
    * fix manifest
    
    * Merge branch graphs into login
    
    * refactor display graphs into functions
    
    * Update colour value
    
    * Update add entry layout
    
    * Add items to spinner and format spacing
    
    * Update navbar and add ListView
    
    * centered text
    
    * refactor buttons and graph into GraphFragment
    
    * load graph onCreate
    
    * Update add entry activity
    
    * Update app flow
    
    Co-authored-by: icextreme <74882450+icextreme@users.noreply.github.com>
    Co-authored-by: Allen Wong <allen@Allens-MacBook-Air-2.local>
    Co-authored-by: Allen Wong <allen@d142-058-087-086.wireless.sfu.ca>
    Co-authored-by: Allen Wong <allen@d207-023-204-121.wireless.sfu.ca>
    Co-authored-by: Allen Wong <allen@Allens-Air-2.hitronhub.home>

--- app/src/main/java/ca/sfu/iat/fintrack/RegisterActivity.kt
+++ app/src/main/java/ca/sfu/iat/fintrack/RegisterActivity.kt
@@ -1,10 +1,9 @@
 package ca.sfu.iat.fintrack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ca.sfu.iat.fintrack.R
 import ca.sfu.iat.fintrack.databinding.ActivityRegisterBinding
-//import ca.sfu.iat.fintrack.databinding.ActivitySigninBinding
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
@@ -13,5 +12,10 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonRegister.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
