c31e48880a73dd35f2556496bbdf2456e4fc2ec3
icextreme <74882450+icextreme@users.noreply.github.com>
2022-03-21 23:30:44 -0700

    Add nested recycler view functionality

---
+++ app/src/main/java/ca/sfu/iat/fintrack/CategoryAdapter.kt
@@ -0,0 +1,30 @@
+package ca.sfu.iat.fintrack
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class CategoryAdapter(private val data: Array<String>) :
+    RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.category_item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.category_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = data[position]
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
--- app/src/main/java/ca/sfu/iat/fintrack/ListAdapter.kt
+++ app/src/main/java/ca/sfu/iat/fintrack/ListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 
@@ -16,10 +17,12 @@ class ListAdapter(private val dataSet: Array<String>) :
      */
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView
+        val recyclerView: RecyclerView
 
         init {
             // Define click listener for the ViewHolder's View.
             textView = view.findViewById(R.id.date_header)
+            recyclerView = view.findViewById(R.id.category_recycler)
         }
     }
 
@@ -38,6 +41,11 @@ class ListAdapter(private val dataSet: Array<String>) :
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
         viewHolder.textView.text = dataSet[position]
+        viewHolder.recyclerView.apply {
+            layoutManager = LinearLayoutManager(viewHolder.recyclerView.context)
+            val dummyItems = arrayOf("Restaurant", "Clothing", "Gas", "Pet")
+            adapter = CategoryAdapter(dummyItems)
+        }
     }
 
     // Return the size of your dataset (invoked by the layout manager)
