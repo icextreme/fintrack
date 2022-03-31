61663330c517d3c0c060e0d6702da33b5d231417
Allen W <awong503@my.bcit.ca>
2022-03-30 22:27:28 -0700

    add update strings from db

--- app/src/main/java/ca/sfu/iat/fintrack/fragments/OverviewFragment.kt
+++ app/src/main/java/ca/sfu/iat/fintrack/fragments/OverviewFragment.kt
@@ -1,6 +1,7 @@
 package ca.sfu.iat.fintrack.fragments
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +12,16 @@ import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import ca.sfu.iat.fintrack.R
 import ca.sfu.iat.fintrack.activities.AddBudgetActivity
+import ca.sfu.iat.fintrack.model.Budget
+import ca.sfu.iat.fintrack.model.Record
 import com.google.android.material.tabs.TabLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -52,6 +62,11 @@ class LandingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // Init Firebase database
+        val database = Firebase.database.reference
+        val uid = FirebaseAuth.getInstance().currentUser?.uid
+
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_overview, container, false)
         tabLayout = view.findViewById(R.id.tabLayout)
@@ -70,6 +85,14 @@ class LandingFragment : Fragment() {
             }
 
         })
+
+        val welcomeUserTextView: TextView = view.findViewById(R.id.textViewWelcome)
+        val username = Firebase.database.reference.child("users/$uid/firstName").get().addOnSuccessListener {
+            welcomeUserTextView.text = String.format(getString(R.string.welcome_user), it.value)
+        }.addOnFailureListener {
+            welcomeUserTextView.text = String.format(getString(R.string.welcome_user), "USER")
+        }
+
         val spinnerPeriod: Spinner = view.findViewById(R.id.spinnerPeriod)
         val spinnerBudget: Spinner = view.findViewById(R.id.spinnerBudget)
 
@@ -108,6 +131,42 @@ class LandingFragment : Fragment() {
             }
 
         }
+
+        val recordsQuery = database.child("users/$uid/records")
+        recordsQuery.addValueEventListener(object: ValueEventListener {
+            val budget = Budget("default")
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var balance: Double = 0.0
+                var expense: Double = 0.0
+                var income: Double = 0.0
+                for (dataSnapshot in snapshot.children) {
+                    val record: Record? = dataSnapshot.getValue<Record>()
+                    if (record != null) {
+                        if (record.type == getString(R.string.income)) {
+                            income += record.amount
+                            balance += record.amount
+                        } else if (record.type == getString((R.string.expense))) {
+                            expense += record.amount
+                            balance -= record.amount
+                        }
+                    }
+                }
+                val balanceTextView :TextView = view.findViewById(R.id.textViewBalance)
+                val expenseTextView :TextView = view.findViewById(R.id.textViewExpense)
+                val incomeTextView :TextView = view.findViewById(R.id.textViewIncome)
+                val str1 = "$ " + String.format("%.2f", balance) + "\nBalance"
+                val str2 = "$ " + String.format("%.2f", expense) + "\nExpense"
+                val str3 = "$ " + String.format("%.2f", income) + "\nIncome"
+                balanceTextView.text = str1
+                expenseTextView.text = str2
+                incomeTextView.text = str3
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("Firebase", "loadPost:onCancelled", error.toException())
+            }
+        })
 
         return view
     }
