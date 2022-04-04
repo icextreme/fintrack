cc54c1742bbe9b832c3a415c900c2be352e2e7b0
truongnguyenlinh <ltruong28@my.bcit.ca>
2022-04-04 12:33:37 -0700

    Complete settings fragment

--- app/src/main/java/ca/sfu/iat/fintrack/fragments/SettingsFragment.kt
+++ app/src/main/java/ca/sfu/iat/fintrack/fragments/SettingsFragment.kt
@@ -1,11 +1,20 @@
 package ca.sfu.iat.fintrack.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import ca.sfu.iat.fintrack.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -19,6 +28,14 @@ private const val ARG_PARAM2 = "param2"
 class SettingsFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
+    private var database: DatabaseReference = Firebase.database.reference
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private var users = database.child("users")
+    private lateinit var firstName: TextInputEditText
+    private lateinit var lastName: TextInputEditText
+    private lateinit var email: TextInputEditText
+    private lateinit var newPassword: TextInputEditText
+    private lateinit var updateButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +50,72 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        firstName = view.findViewById(R.id.input_first_name_editText)
+        lastName = view.findViewById(R.id.input_last_name_editText)
+        email = view.findViewById(R.id.input_email_editText)
+        newPassword = view.findViewById(R.id.input_new_password_editText)
+        updateButton = view.findViewById(R.id.button_update)
+
+        val user = auth.currentUser
+        val uid = user?.uid
+        val databaseRef = Firebase.database.reference.child("users/$uid")
+
+        updateButton.setOnClickListener {
+            if (!firstName.text.isNullOrBlank()) {
+                databaseRef.child("firstName")
+                    .setValue(firstName.text.toString())
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d("firstNameUpdated::", "Success")
+                            Toast.makeText(
+                                context, "Successfully updated first name",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            }
+
+            if (!lastName.text.isNullOrBlank()) {
+                databaseRef.child("lastName")
+                    .setValue(lastName.text.toString())
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d("lastNameUpdated::", "Success")
+                            Toast.makeText(
+                                context, "Successfully updated last name",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            }
+
+            if (!email.text.isNullOrBlank()) {
+                databaseRef.child("email")
+                    .setValue(email.text.toString())
+                user?.updateEmail(email.text.toString())
+                    ?.addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d("emailUpdated::", "Success")
+                            Toast.makeText(
+                                context, "Successfully updated email",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            }
+
+            if (!newPassword.text.isNullOrBlank()) {
+                user?.updatePassword(newPassword.text.toString())
+                    ?.addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d("passwordUpdated::", "Success")
+                        }
+                    }
+            }
+        }
+        return view
     }
 
     companion object {
